a8edef22998c353e78fd9ba27ffbd05a7518e070
JYCestelle <yichej@uw.edu>
2016-12-06 01:54:21 -0800

    delete extra code

--- app.js
+++ app.js
@@ -12,18 +12,6 @@ $('#jello').on('click', function(){
     $('#image').addClass('animated jello');
 });
 
-//jQuery extension, creating a method to handle adding animation class and then removing upon end
-// $('#yourElement').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', doSomething);
-
-$.fn.extend({
-    animateCss: function (animationName) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        this.addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-        });
-    }
-});
-
 /* creating a method to handle how to add animation class without reload page. We should use this function to remove it at the end
 var end = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 $("#jello").on('click', function(){
