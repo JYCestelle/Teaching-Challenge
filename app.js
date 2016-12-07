686cbaecac2fefcbc4bae8cab6fbcd93a7648f05
JYCestelle <yichej@uw.edu>
2016-12-07 03:02:26 -0800

    fixed code

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ $('#jello').on('click', function(){
 /* creating a method to handle how to add animation class without reload page. We should use this function to remove it at the end
 var end = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 $("#jello").on('click', function(){
-    $("#image").addClass('animated jello'.one(end, function(){
+    $("#image").addClass('animated jello').one(end, function(){
         $(this).removeClass('animated jello');
     }));
 });*/
